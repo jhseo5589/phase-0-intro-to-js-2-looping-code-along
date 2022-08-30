d3cfc1a3114c50ec5a3eb3bbdfa194f5c686cd07
Jong Hyun Seo <jhseo5589@gmail.com>
2022-08-30 13:51:15 -0400

    commit tree 480beb7fa301392a375d3d941a6561188a2f4d6e

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+const names = ["Guadalupe", "Ollie", "Arki"];
+
+function writeCards(names) {
+    let fullList = []
+    for (let i = 0; i < names.length; i++) {
+        fullList.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        
+    }
+    return fullList;
+}
+writeCards(names);
+
+
+
+function countDown() {
+    for (let countDown = 0; countDown < 10; countDown++) {
+    }
+        console.log(countDown);
+    }
